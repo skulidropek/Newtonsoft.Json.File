d4e7d1345c172eb65fc3036ed30b62746371abbc
Skuli Dropek <66840575+skulidropek@users.noreply.github.com>
2023-10-23 22:37:04 +0300

    Init

--- Newtonsoft.Json.File/JsonFileSerializer.cs
+++ Newtonsoft.Json.File/JsonFileSerializer.cs
@@ -5,22 +5,22 @@ namespace Newtonsoft.Json.File
 {
     public class JsonFileSerializer
     {
-        public static T ReadFileToJson<T>(string path)
+        public static T Deserialize<T>(string path)
         {
             return JsonConvert.DeserializeObject<T>(System.IO.File.ReadAllText(path));
         }
-        
-        public static void WriteFileToJson<T>(string path, T t)
+
+        public static void Serialize<T>(string path, T t)
         {
             System.IO.File.WriteAllText(path, JsonConvert.SerializeObject(t));
         }
-        
-        public static async Task<T> ReadFileToJsonAsync<T>(string path)
+
+        public static async Task<T> DeserializeAsync<T>(string path)
         {
             return JsonConvert.DeserializeObject<T>(await System.IO.File.ReadAllTextAsync(path));
         }
-        
-        public static async Task WriteFileToJsonAsync<T>(string path, T t)
+
+        public static async Task SerializeAsync<T>(string path, T t)
         {
             await System.IO.File.WriteAllTextAsync(path, JsonConvert.SerializeObject(t));
         }
